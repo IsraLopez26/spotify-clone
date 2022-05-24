8b23ce5b4346043066defc8eda4ee718632f7efc
IsraLopez <isralop26@gmail.com>
2022-05-24 13:47:21 -0500

    Added Sidebar styling and logo

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -4,13 +4,13 @@ import Sidebar from './sidebar';
 const PlayerLayout = ({children})=>{
   return(
     <Box width='100vw' height='100vh'>
-      <Box position='absolute' top='0' width='250px' left='0' backgroundColor='blue'>
+      <Box position='absolute' top='0' width='250px' left='0'>
         <Sidebar/>
       </Box>
-      <Box marginLeft='250px' marginBottom='100px' backgroundColor='red'>
+      <Box marginLeft='250px' marginBottom='100px'>
         {children}
       </Box>
-      <Box position='absolute' left='0' bottom='0' backgroundColor='yellow'>
+      <Box position='absolute' left='0' bottom='0'>
         player
       </Box>
     </Box>
