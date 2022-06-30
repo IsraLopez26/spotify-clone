f0c1c7276e3db31503323e24e365e8061fea9bc0
IsraLopez <isralop26@gmail.com>
2022-06-30 13:51:30 -0500

    Added titles to the gradient layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import GradientLayout from "../components/gradientLayout";
 
 const Home =() =>{
   return (
-   <GradientLayout color='blue'>
+   <GradientLayout color='blue' subtitle='profile' title='Isra Lopez' description='15 public playlists'>
     <div>KK</div>
    </GradientLayout>
   )
